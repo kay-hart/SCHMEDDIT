ea56498cc25f651c0fa84d2cf5bc002c368978e6
kay-hart <36796030+kay-hart@users.noreply.github.com>
2018-02-25 01:15:47 -0600

    Initial commit

---
+++ app/src/main/java/com/example/kaylie/firstapplicationpractice/Frag1.java
@@ -0,0 +1,10 @@
+package com.example.kaylie.firstapplicationpractice;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Kaylie on 2/24/2018.
+ */
+
+public class Frag1 extends Fragment {
+}
