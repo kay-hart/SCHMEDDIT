52f8975b0c15202f3f8fea816aef8ee4171ecc71
cdyrssen <cdyrssen@gmail.com>
2018-02-27 22:22:24 -0600

    Build fix
    Fixed build errors. It now runs in emulator.

--- app/src/main/java/com/example/kaylie/firstapplicationpractice/MainActivity.java
+++ app/src/main/java/com/example/kaylie/firstapplicationpractice/MainActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ActionBarContainer;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,12 +22,6 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        mToggle.onOptionsItemSelected(){
-
-        }
-
-        
     }
 
     @Override
