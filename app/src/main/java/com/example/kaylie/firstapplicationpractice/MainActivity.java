c9520d0efad0e1ea5511addb3f212fcfeb5b3fbc
cdyrssen <cdyrssen@gmail.com>
2018-03-03 14:54:44 -0600

    Navigation menu event handler added
    Added log entries whenever any of the navigation menu items are selected. If a menu item is pressed, the navigation bar is then hidden again.

--- app/src/main/java/com/example/kaylie/firstapplicationpractice/MainActivity.java
+++ app/src/main/java/com/example/kaylie/firstapplicationpractice/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.kaylie.firstapplicationpractice;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.util.Log;
+
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,7 +21,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        NavigationView navVeiw = findViewById(R.id.nav_view);
+        navVeiw.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+                        switch (item.getItemId()) {
+                            case R.id.profile:
+                                Log.d("Navmenu", "profile pressed");
+                                break;
+                            case R.id.lists:
+                                //setContentView(R.layout.activity_list_tabs);
+                                Log.d("Navmenu", "lists pressed");
+                                break;
+                            case R.id.leaderboard:
+                                Log.d("Navmenu", "leaderboards pressed");
+                                break;
+                            case R.id.settings:
+                                Log.d("Navmenu", "settings pressed");
+                                break;
+                            case R.id.about:
+                                Log.d("Navmenu", "about pressed");
+                                break;
+                            case R.id.help:
+                                Log.d("Navment", "help pressed");
+                                break;
+                            default:
+                                Log.d("mytag", "Test");
+                        }
+                        return true;
+                    }
+                }
+        );
+
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
@@ -26,9 +68,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-      if(mToggle.onOptionsItemSelected(item)) {
-          return true;
-      }
+        if(mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
